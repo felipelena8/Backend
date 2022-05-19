3e42ee278cda1daf0bb59efda2184e41cf7229dd
felipelena8 <77079122+felipelena8@users.noreply.github.com>
2022-05-19 13:31:08 -0300

    Add files via upload

---
+++ HANDLEBARS/api.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const {engine} = require("express-handlebars");
+const ContenedorProductos = require("./ContenedorProductos");
+const { Router } = express;
+
+
+const containerProds = new ContenedorProductos();
+const productosRouter = Router();
+const app = express();
+
+const engineFn = engine({
+    extname: '.hbs',
+    defaultLayout: `${__dirname}/views/index.hbs`,
+    layoutsDir: `${__dirname}/views/layouts`,
+    partialsDir: `${__dirname}/views/partials`
+  })
+  
+app.engine('hbs', engineFn)
+app.set('views', './views')
+app.set('view engine', 'hbs')
+  
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("", express.static(__dirname + "/public"));
+productosRouter.get("", (req, res) => {
+  res.json(containerProds.productos());
+});
+productosRouter.get("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const prod = containerProds.buscarProducto(id);
+  if (prod === undefined) {
+    return res.json({ error: "producto no encontrado" });
+  }
+  return res.json(prod);
+});
+productosRouter.post("", (req, res) => {
+  console.log("Request post");
+  const newProd = req.body;
+  let finalProd = containerProds.agregarProducto(newProd);
+  return res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get("/productos", (req, res) => {
+    const prods = {productos: containerProds.productos()}
+    return res.render('layouts/productos',prods)
+});
+
+productosRouter.delete("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  let seElimino = containerProds.eliminarProducto(id);
+  if (!seElimino) {
+    return res.send("El producto no se elimino o no existe");
+  }
+  return res.send("Se elimino correctamente");
+});
+
+productosRouter.put("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const newProducto = req.body;
+  const actualizado = containerProds.actualizarProducto(id, newProducto);
+  res.send(
+    `El producto ${actualizado ? "se actualizo correctamente" : "no existe"}`
+  );
+});
+
+app.use("/api/productos", productosRouter);
+
+const server = app.listen(8080, () => {
+  console.log("Servidor HTTP corriendo en puerto 8080");
+});
+server.on("error", (error) => console.log(`Ha ocurrido un error: ${error}`));
---
+++ PUG/api.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const ContenedorProductos = require("./ContenedorProductos");
+const { Router } = express;
+
+const containerProds = new ContenedorProductos();
+const productosRouter = Router();
+const app = express();
+
+  
+app.set('views', './views')
+app.set('view engine', 'pug')
+  
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("", express.static(__dirname + "/public"));
+productosRouter.get("", (req, res) => {
+  res.json(containerProds.productos());
+});
+productosRouter.get("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const prod = containerProds.buscarProducto(id);
+  if (prod === undefined) {
+    return res.json({ error: "producto no encontrado" });
+  }
+  return res.json(prod);
+});
+productosRouter.post("", (req, res) => {
+  console.log("Request post");
+  const newProd = req.body;
+  let finalProd = containerProds.agregarProducto(newProd);
+  return res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get("/productos", (req, res) => {
+    const prods = {productos: containerProds.productos()}
+    return res.render('layouts/productos',prods)
+});
+
+productosRouter.delete("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  let seElimino = containerProds.eliminarProducto(id);
+  if (!seElimino) {
+    return res.send("El producto no se elimino o no existe");
+  }
+  return res.send("Se elimino correctamente");
+});
+
+productosRouter.put("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const newProducto = req.body;
+  const actualizado = containerProds.actualizarProducto(id, newProducto);
+  res.send(
+    `El producto ${actualizado ? "se actualizo correctamente" : "no existe"}`
+  );
+});
+
+app.use("/api/productos", productosRouter);
+
+const server = app.listen(8080, () => {
+  console.log("Servidor HTTP corriendo en puerto 8080");
+});
+server.on("error", (error) => console.log(`Ha ocurrido un error: ${error}`));
